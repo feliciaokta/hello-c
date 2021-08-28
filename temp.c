ca735df051d58e1b704b7ef12c9236dfe079ce4e
Felicia Okta <feliciaokta@outlook.com>
2021-08-28 14:20:28 -0700

    write formula

--- temp.c
+++ temp.c
@@ -2,3 +2,20 @@
 
 // print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300
 
+int main () {
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;    // lower limit of temp table
+  upper = 300;  // upper limit
+  step = 20;    // increment size
+
+  fahr = lower;
+
+  while (fahr <= upper) {
+    celsius = 5 * (fahr-32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr += step;
+  }
+
+}
