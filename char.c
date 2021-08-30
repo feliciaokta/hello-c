56439e6fd464a6482c5d289dc4b0c48dce5b1639
Felicia Okta <feliciaokta@outlook.com>
2021-08-29 21:03:08 -0700

    refactor getchar putchar

--- char.c
+++ char.c
@@ -1,15 +1,31 @@
 // getchar reads the next input character
 // putchar prints a character when called
+// the following programs are used to copy input into output
 
 #include <stdio.h>
 
 main() {
   int c;
 
-  c = getchar();
+  c = getchar();        // read a character
   
-  while (c != EOF) {    // EOF = end of file
+  // while it's not the end of the file, output the character that was just read
+  // EOF = end of file, built-in from stdio.h
+  while (c != EOF) {
     putchar(c);
     c = getchar();
   }
+}
+
+
+
+// another way to write it
+#include <stdio.h>
+
+main() {
+  int c;
+
+  // notice this while doesn't have curly brackets
+  while ((c = getchar()) != EOF)
+    putchar(c);
 }
