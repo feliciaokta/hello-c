8f2e0af93f4e85533d77e09735e8847bc481512d
Felicia Okta <feliciaokta@outlook.com>
2021-09-10 21:13:53 -0700

    count blanks

--- charcount.c
+++ charcount.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 
-// this program counts the amount of characters
-// the count is saved in long integer "nc"
+// // this program counts the amount of characters
+// // the count is saved in long integer "nc"
+// int main ()
+// {
+//   long nc;
+
+//   nc = 0;
+
+//   while (getchar() != EOF)
+//     ++nc;       // you can also write nc++;
+//   printf("%ld\n", nc);
+// }
+
+
+
+// // we can also write that using a for loop
+// int main ()
+// {
+//   double nc;  // store decimal numbers with 2 decimal points, vs float which only stores one decimal number
+
+//   for (nc = 0; getchar() != EOF; nc++)
+//     ;                     // the body of the loop is empty
+//   printf("%.0f\n", nc);   // percent dot zero f, means it prints zero decimal points
+// }
+
+
+
+// write a program to count blanks, tabs, and newlines
 int main ()
 {
   long nc;
+  int blanks = 0;
+  int tabs = 0;
+  int newlines = 0;
 
-  nc = 0;
+  for (nc = 0; getchar() != EOF; nc++) {
 
-  while (getchar() != EOF)
-    ++nc;       // you can also write nc++;
-  printf("%ld\n", nc);
-}
+    if (nc == ' ') {
+      blanks++;
+    }
+  } 
 
-// we can also write that using a for loop
-int main ()
-{
-  double nc;
-
-  for (nc = 0; getchar() != EOF; nc++)
-    ;                     // the body of the loop is empty
-  printf("%.0f\n", nc);   // percent dot zero f, means it prints zero decimal points
 }
