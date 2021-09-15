9d58d85f8a5972f2617aaf23f65d2ec257bbaa3c
Felicia Okta <feliciaokta@outlook.com>
2021-09-14 23:47:57 -0700

    not sure what to do

--- input_copy_output.c
+++ input_copy_output.c
@@ -8,6 +8,8 @@ int main ()
   char input = getchar();
   char output = putchar(input);
 
+  // try to see if output works an exact copy first
+
   // while input != EOF
   // if character = blank && character length > 1
   // then output ' '
