23163f02ea0401370d3a893136ca3689a5920b79
Felicia Okta <feliciaokta@outlook.com>
2021-09-14 23:28:37 -0700

    copy input , single blank, pseudocode

---
+++ input_copy_output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// write a program to copy its input into an output,
+// replacing each string of one or more blanks by a single blank
+
+int main ()
+{
+  char input = getchar();
+  char output = putchar(input);
+
+  // while input != EOF
+  // if character = blank && character length > 1
+  // then output ' '
+
+}
